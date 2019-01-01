c6f76c27ba86ddb310acc863a03d113380f4444e
Samuel Opoku-Agyemang <samuel.opokuagyemang@gmail.com>
2019-01-01 05:56:53 -0500

    sam:fixing integer-float bug for python 3.6

--- 2019/Snake-Game-Python/Snake.py
+++ 2019/Snake-Game-Python/Snake.py
@@ -28,8 +28,8 @@ w.timeout(100)
 maxScore = 0
 scores = []
 score = 0
-snk_x = sw/4
-snk_y = sh/2
+snk_x = sw//4
+snk_y = sh//2
 snake = [
     [snk_y, snk_x],
     [snk_y, snk_x-1],
@@ -37,7 +37,7 @@ snake = [
     [snk_y, snk_x-3]
 ]
 
-food = [sh/2, sw/2]
+food = [sh//2, sw//2]
 w.addch(food[0], food[1], curses.ACS_PI)
 
 key = curses.KEY_RIGHT
@@ -59,7 +59,7 @@ while True:
 
     prevKey = key
     #Increases the speed of Snake as its length increases
-    w.timeout(100 - (len(snake)/5 + len(snake)/10)%120)
+    w.timeout(100 - (len(snake)//5 + len(snake)//10)%120)
 
     next_key = w.getch()
     key = key if next_key == -1 else next_key
@@ -79,14 +79,14 @@ while True:
 
 ### Students are responsible for implementing the commented out  code
 
-    # if key == curses.KEY_DOWN:
-    #     new_head[0] += 1
-    # if key == curses.KEY_UP:
-    #     new_head[0] -= 1
-    # if key == curses.KEY_LEFT:
-    #     new_head[1] -= 1
-    # if key == curses.KEY_RIGHT:
-    #     new_head[1] += 1
+    if key == curses.KEY_DOWN:
+        new_head[0] += 1
+    if key == curses.KEY_UP:
+        new_head[0] -= 1
+    if key == curses.KEY_LEFT:
+        new_head[1] -= 1
+    if key == curses.KEY_RIGHT:
+        new_head[1] += 1
 
     ### DO NOT CHANGE CODE BELOW
     snake.insert(0, new_head)
@@ -118,5 +118,6 @@ while True:
         w.addch(food[0], food[1], curses.ACS_PI)
     else:
         tail = snake.pop()
+        print(snake)
         w.addch(tail[0], tail[1], ' ')
     w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
