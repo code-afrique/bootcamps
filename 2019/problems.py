18c3b2deb1c7b2ff6a9fb4c4dc85e1838d4e72b8
Karan Newatia <kn348@cornell.edu>
2019-01-10 23:34:04 +0000

    Robot

--- 2019/problems.py
+++ 2019/problems.py
@@ -20,11 +20,14 @@
 	This function reverses the characters in a string.
 	Example: reverse('hello') should return 'olleh'
 	
-4. There is a robot starting at position (0, 0), the origin, on a 2D plane.
+4.  write function robot_return_to_origin(str).
+
+    There is a robot starting at position (0, 0), the origin, on a 2D plane.
     Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
     The move sequence is represented by a string, and the character moves[i] represents its ith move.
     Valid moves are R (right), L (left), U (up), and D (down). 
     If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
+    
     
     Example 1:
     Input: "UD"
