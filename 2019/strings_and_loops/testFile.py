d25b234fadf67fe1e9ea141b9235022fc494b681
askatako <askatako@gmail.com>
2019-01-10 23:20:53 +0000

    updated testFIle

--- 2019/strings_and_loops/testFile.py
+++ 2019/strings_and_loops/testFile.py
@@ -1,11 +1,29 @@
 from code_Afrique_soln_Sena import *
+from words import words1, words2
 import introcs
 
-introcs.assert_equals(11, countString(words1, "foreign"))
-introcs.assert_equals(9, countString(words1, "challenge"))
-introcs.assert_equals(0, countString(words2, "happy"))
-introcs.assert_equals(1, countString(words2, "individual"))
+def checkCountString():
+
+	introcs.assert_equals(11, countString(words1, "foreign"))
+	introcs.assert_equals(9, countString(words1, "challenge"))
+	introcs.assert_equals(0, countString(words2, "happy"))
+	introcs.assert_equals(1, countString(words2, "individual"))
 
 
-print("Function succeeded")
+	print("countString Function succeeded")
 
+checkCountString()
+
+
+def checkReverseString():
+
+	introcs.assert_equals("", reverseString(""))
+	introcs.assert_equals("nac", reverseString("can"))
+	introcs.assert_equals("yppah", reverseString("happy"))
+	introcs.assert_equals("laudividni", reverseString("individual"))
+
+
+	print("reverseString Function succeeded")
+
+
+checkReverseString()
