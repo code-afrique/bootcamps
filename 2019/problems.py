369dc4f0f4ca93c8e6f1a79b1d552bf7c84f2106
Robbert <rvr@cs.cornell.edu>
2019-01-10 23:13:17 +0000

    Adding problems.py

---
+++ 2019/problems.py
@@ -0,0 +1,17 @@
+1. write function quadratic_fun(a, b, c).
+	This function checks the roots of a quadratic equation of the form 
+	ax^2 + bx + c = 0
+
+	Tasks:
+	1. First find the discriminant of the equation by using:
+	 	 discriminant = b*b - 4*a*c
+	2. If the discriminant is > 0 return the string 'real roots' 
+			Else if the discriminant is = 0 return string 'equal roots'
+	    Else if the discriminant is < 0 returnthe string 'complex roots'
+
+	Hint: Use if-statements
+	Example: quadratic_fun(1, 2, 1) should return 'equal roots"
+
+2. write function find_minimum(lst).
+	This function finds the minimum number in a list of numbers.
+	Example: find_minimum([2, 1, 3]) should return 1
