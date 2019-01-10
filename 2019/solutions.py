1b5fdec4b5242ecc70f1ea47a7fb6a69900d9d8b
Karan Newatia <kn348@cornell.edu>
2019-01-10 23:55:30 +0000

    solution q2

--- 2019/solutions.py
+++ 2019/solutions.py
@@ -10,3 +10,15 @@ def robot_return_to_origin(str):
         elif move == 'R': x += 1
 
     return x == y == 0
+
+
+#Solution for question 2:
+def find_minimum(lst):
+
+    x = lst[0]
+
+    for i in range(1,len(lst)):
+        if lst[i] < x:
+            x = lst[i]
+            
+    return x
