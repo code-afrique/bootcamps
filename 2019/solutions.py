eaca5820cf07a51e03ce7ff9b2d54cb83467ac5c
Karan Newatia <kn348@cornell.edu>
2019-01-11 00:05:59 +0000

    soln q3

--- 2019/solutions.py
+++ 2019/solutions.py
@@ -14,11 +14,22 @@ def robot_return_to_origin(str):
 
 #Solution for question 2:
 def find_minimum(lst):
+    if lst==[]:
+        return False
 
     x = lst[0]
 
     for i in range(1,len(lst)):
         if lst[i] < x:
             x = lst[i]
-            
+
+    return x
+
+#Solution for question 3:
+def reverse(str):
+
+    n = len(str)
+    x=""
+    for i in range(n-1,-1,-1):
+        x += test[i]
     return x
