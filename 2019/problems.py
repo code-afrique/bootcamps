ba7f7983538ef340a0dcd46f14f0845b8f9c21bc
Karan Newatia <kn348@cornell.edu>
2019-01-11 02:04:52 +0000

    all solutions

--- 2019/problems.py
+++ 2019/problems.py
@@ -1,11 +1,11 @@
 1. write function quadratic_fun(a, b, c).
-	This function checks the roots of a quadratic equation of the form 
+	This function checks the roots of a quadratic equation of the form
 	ax^2 + bx + c = 0
 
 	Tasks:
 	1. First find the discriminant of the equation by using:
 	 	 discriminant = b*b - 4*a*c
-	2. If the discriminant is > 0 return the string 'real roots' 
+	2. If the discriminant is > 0 return the string 'real roots'
 			Else if the discriminant is = 0 return string 'equal roots'
 	    Else if the discriminant is < 0 returnthe string 'complex roots'
 
@@ -20,20 +20,20 @@
 3. write function reverse(str).
 	This function reverses the characters in a string.
 	Example: reverse('hello') should return 'olleh'
-	
+
 4.  write function robot_return_to_origin(str).
     There is a robot starting at position (0, 0), the origin, on a 2D plane.
     Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
     The move sequence is represented by a string, and the character moves[i] represents its ith move.
-    Valid moves are R (right), L (left), U (up), and D (down). 
+    Valid moves are R (right), L (left), U (up), and D (down).
     If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
-    
-    
+
+
     Example 1:
     Input: "UD"
-    Output: true 
+    Output: true
     Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
-    
+
     Example 2:
     Input: "LL"
     Output: false
@@ -63,6 +63,6 @@
 	Example 2: alternates([5, 3, 4, 2, 6]) should return true.
 	Example 3: alternates([5, 3, 4, 5, 2]) should return false.
 
-10. write function nOccurrences(str1, str2).
+10. write function nOccurences(str1, str2).
 	This function returns how many times str2 occurs in str1.
 	Example nOccurences('xxxx', 'xx') should return 3.
--- 2019/solutions.py
+++ 2019/solutions.py
@@ -1,16 +1,3 @@
-#Solution for question 4:
-def robot_return_to_origin(str):
-
-    x = y = 0
-
-    for move in str:
-        if move == 'U': y -= 1
-        elif move == 'D': y += 1
-        elif move == 'L': x -= 1
-        elif move == 'R': x += 1
-
-    return x == y == 0
-
 
 #Solution for question 2:
 def find_minimum(lst):
@@ -31,5 +18,87 @@ def reverse(str):
     n = len(str)
     x=""
     for i in range(n-1,-1,-1):
-        x += test[i]
+        x += str[i]
     return x
+
+#Solution for question 4:
+def robot_return_to_origin(str):
+
+    x = y = 0
+
+    for move in str:
+        if move == 'U': y -= 1
+        elif move == 'D': y += 1
+        elif move == 'L': x -= 1
+        elif move == 'R': x += 1
+
+    return x == y == 0
+
+#Solution for question 5:
+def find_second_highest_number(lst):
+
+    if len(lst) < 2:
+        return False
+
+    largest = lst[0]
+    largest2 = lst[0]
+
+    for item in lst:
+        if item > largest:
+            largest = item
+        elif largest2!=largest and largest2 < item:
+                largest2 = item
+
+    return largest2
+
+#Solution for question 6:
+def list_of_even_numbers(start, end):
+
+    result = []
+    for i in range(start,end):
+        if i % 2 == 0:
+            result.append(i)
+
+    return result
+
+#Solution for question 7:
+def doubleList(lst):
+
+    for i in range(len(lst)):
+        lst[i]*= 2
+
+    return lst
+
+#Solution for question 8:
+def isPalindrome(lst):
+
+    if len(lst)<2:
+        return True
+    return (lst[0]==lst[-1]) and isPalindrome(lst[1:-1])
+
+#Solution for question 9:
+def alternates(lst):
+
+    dir = lst[0] > lst[1]
+
+    for i in range(len(lst)-1):
+        if dir:
+            if not (lst[i] > lst[i+1]):
+                return False
+        else:
+            if not (lst[i] < lst[i+1]):
+                return False
+
+        dir = not dir
+
+    return True
+
+#Solution for question 10:
+def nOccurrences(str1, str2):
+
+    result = 0
+    for i in range(len(str1)):
+        if str1[i:i+len(str2)] == str2:
+            result+=1
+
+    return result
